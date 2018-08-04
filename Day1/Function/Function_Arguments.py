c29a2f0ad9729d9be57b73eeedb07337de11b52d
Rautela Arjun <arautela@gemalto.com>
2018-08-04 18:23:47 +0530

    Function Arguments

---
+++ Day1/Function/Function_Arguments.py
@@ -0,0 +1,34 @@
+#Describing how arguments are passed to function
+
+## Immutable object passed to function arguments.
+
+def sum(a,b):
+    print("Id of a :", id(a))
+    print("Id of b :", id(b)) 
+    a = 20
+    print("Id of a after update:", id(a))
+    total = a + b
+    return total
+
+x = 5
+y = 10
+
+print("Id of x : ", id(x))
+print("Id of y : ", id(y))
+
+sum_x_y = sum(x,y)
+print('Sum of x and y', sum_x_y)
+
+
+
+## Mutable object passed to it
+# function that accept list as input and append a value to it
+
+def list_func(name_list):
+    print("Original List : ", name_list)
+    name_list.append('Kevin')
+
+min_name = ['Bob', 'stuart']
+
+list_func(min_name)
+print("min_name list after function call : ", min_name)

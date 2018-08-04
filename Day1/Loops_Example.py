76834641b3849e7e4161fec929fa687d46e6b370
Rautela Arjun <arautela@gemalto.com>
2018-08-04 13:16:16 +0530

    arjunrautela@gmail.com

---
+++ Day1/Loops_Example.py
@@ -0,0 +1,19 @@
+#Loop Example in python
+
+#For Loop
+
+name = 'Arjun'
+
+for ch in name:
+    print("Ch : ", ch, end='\t')
+else:
+    print("\nFor loop is finished")
+
+
+#While loop Example:
+index = 0
+while index < len(name):
+    print("Ch : ", name[index])
+    index = index + 1
+else:
+    print("While loop Finished")

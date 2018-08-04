482af1792c31f23de556507e18f7ab72d9d55f8f
Rautela Arjun <arautela@gemalto.com>
2018-08-04 18:11:17 +0530

    Function Example

---
+++ Day1/Function/Functions_Example.py
@@ -0,0 +1,18 @@
+#Function example
+
+#The function object may be assigned to other names
+def sum(a,b):
+    return a+b
+
+def mul(a,b):
+    return a*b
+
+print ("Mul : ", mul(2,3))
+
+x = mul
+print("X Mul : ", x(2,3))
+
+# List consisting of function name
+
+func_list = [sum, mul]
+print(func_list[0](2,3))

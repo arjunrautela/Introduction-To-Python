b08586cb2045e8e115622a5270984a22177688fc
Rautela Arjun <arautela@gemalto.com>
2018-08-04 12:34:57 +0530

    PP1_1 file added

---
+++ Day1/PP1_1.py
@@ -0,0 +1,17 @@
+#Python arithmetic operation
+#Ctrl+Shift+P then select python interpretor
+
+n1 = 10
+n2 = 3
+
+print("Sum : ", n1 + n2)
+
+print ("subtract : ", n1 - n2)
+
+print("Multiply : ", n1*n2)
+
+print("Division : ", n1/n2)
+
+print ("Integer division : ", n1//n2)
+
+print ("Reminder : ", n1%n2)

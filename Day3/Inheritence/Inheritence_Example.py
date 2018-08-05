38949b92581b4e97a9eb1c9f98f7a643ace28bc5
Rautela Arjun <arautela@gemalto.com>
2018-08-05 20:30:53 +0530

    Simple Inhertince Example

---
+++ Day3/Inheritence/Inheritence_Example.py
@@ -0,0 +1,21 @@
+# Inheritence Example
+
+class emp:
+    def __init__(self, val):
+        self.val = val
+    
+    def display(self):
+        print(self.val)
+
+
+class special_emp(emp):
+    def display(self):
+        print('I am a special emp', self.val)
+
+
+
+
+
+if __name__ == "__main__":
+    sp_emp = special_emp('Arjun')
+    sp_emp.display()

a861aa9229341ba3240ef82c70780291edccd26a
Rautela Arjun <arautela@gemalto.com>
2018-08-05 14:30:38 +0530

    Lambda expression for grabbing the first character of a string

---
+++ Day2/Practice Problem 6/PP6_1.py
@@ -0,0 +1,9 @@
+#Lambda expression for grabbing the first character of a string
+
+first_charcter = lambda data_string : data_string[0]
+
+print('First Character : ', first_charcter('Arjun'))
+
+print('First Character : ', first_charcter('Singh'))
+
+print('First Character : ', first_charcter('Rautela'))

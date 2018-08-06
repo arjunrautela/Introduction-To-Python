e4ab45fcb76b9e9f1cf115d507afa35554ad3eed
Rautela Arjun <arautela@gemalto.com>
2018-08-06 08:41:26 +0530

    Recursive Function

---
+++ Day1/Function/Func_recursive.py
@@ -0,0 +1,13 @@
+# Example of Recursive Function
+
+def factorial_recursive(n):
+    # Base case: 1! = 1
+    if n == 1:
+        return 1
+
+    # Recursive case: n! = n * (n-1)!
+    else:
+        return n * factorial_recursive(n-1)
+
+
+print('Factorial : ', factorial_recursive(5))
